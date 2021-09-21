08ccce3e3955e5498313c5bc2e95122fadbcb8c1
GMDEEP <stewartjgreen@gmail.com>
2021-09-21 17:28:23 +0000

    updated navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,11 +5,20 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Home</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<Link to="/about">
+					<button className="btn btn-primary">ABOUT</button>
+				</Link>
+				<Link to="/portfolio">
+					<button className="btn btn-primary">PORTFOLIO</button>
+				</Link>
+				<Link to="/blog">
+					<button className="btn btn-primary">BLOG</button>
+				</Link>
+				<Link to="/contact">
+					<button className="btn btn-primary">CONTACT</button>
 				</Link>
 			</div>
 		</nav>
